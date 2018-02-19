c17554e69d0c986ade6adf747ac74c0955530ba2
Anton Glushkov <alexandrovich.anton7@gmail.com>
2018-02-19 22:02:47 +0400

    added limitations to randomQuestionService

--- src/main/java/com/dev/service/RandomQuestionService.java
+++ src/main/java/com/dev/service/RandomQuestionService.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import java.io.IOException;
 
 public class RandomQuestionService {
-    private static final String URL = "https://db.chgk.info/xml/random/limit1";
+    private static final String URL = "https://db.chgk.info/xml/random/types1/from_2014-01-01/limit1";
 
     public Question retrieve() {
         XmlMapper mapper = new XmlMapper();
